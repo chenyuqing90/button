7d24e6e29bfb92dfa960636a8e61189fcee65868
chenyuqing90 <408630399@gms.tku.edu.tw>
2022-11-12 15:28:02 +0800

    button

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,48 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(App());
+void main() => runApp(const SnackBarDemo());
 
-class App extends StatelessWidget{
+class SnackBarDemo extends StatelessWidget {
+  const SnackBarDemo({super.key});
+
   @override
-  Widget build(BuildContext context){
-    //建立appTitle物件
-    var appTitle = Text('Flutter Button');
-
-    //建立appBody物件
-    var appBody = AppBody();
-
-    //建立appBar物件
-    var appBar = AppBar(
-      title: appTitle,
-    );
-
-    //建立app物件
-    var app = MaterialApp(
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SnackBar Demo',
       home: Scaffold(
-        appBar: appBar,
-        body: appBody,
+        appBar: AppBar(
+          title: const Text('SnackBar Demo'),
+        ),
+        body: const SnackBarPage(),
       ),
     );
-
-    return app;
   }
 }
 
+class SnackBarPage extends StatelessWidget {
+  const SnackBarPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          final snackBar = SnackBar(
+            content: const Text('Yay! A SnackBar!'),
+            action: SnackBarAction(
+              label: 'Undo',
+              onPressed: () {
+                // Some code to undo the change.
+              },
+            ),
+          );
+
+          // Find the ScaffoldMessenger in the widget tree
+          // and use it to show a SnackBar.
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        },
+        child: const Text('Show SnackBar'),
+      ),
+    );
+  }
+}
